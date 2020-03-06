dc36426d1f0649ad2863b426d328323e9c032976
Simon Asholt Norup <wormple12@hotmail.com>
2020-03-06 15:11:02 +0100

    Period2 Day1: Facade UserAPI + REST-API

--- Period2/Day1/express-intro/src/app.ts
+++ Period2/Day1/express-intro/src/app.ts
@@ -2,17 +2,65 @@ require("dotenv").config(); // ENVIRONMENT VARIABLES
 import express from "express";
 import path from "path";
 const app = express();
+const Joi = require("joi");
 const helmet = require("helmet"); // SECURITY
 import UserFacade from "./facades/userFacade";
 
 app.use(helmet());
 
+app.use(express.json());
 app.use(express.static(path.join(process.cwd(), "public")));
 // add security middleware
 
 app.get("/api/users", (req, res) => {
   const users = UserFacade.getAllUsers();
   res.json(users);
+});
+
+app.get("/api/user/:username", (req, res) => {
+  try {
+    const user = UserFacade.getUser(req.params.username);
+    res.json(user);
+  } catch (Error) {
+    res.status(404).send(Error.message);
+  }
+});
+
+app.post("/api/user", (req, res) => {
+  const requirements = {
+    name: Joi.string().required(),
+    userName: Joi.string().required(),
+    password: Joi.string().required(),
+    role: Joi.string().required()
+  };
+  const result = Joi.validate(req.body, requirements);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const wasAdded = UserFacade.addUser(result.value);
+  res.send(`${result.value.userName} was added: ${wasAdded}`);
+});
+
+app.delete("/api/user/:username", (req, res) => {
+  try {
+    const wasDeleted = UserFacade.deleteUser(req.params.username);
+    res.send(`${req.params.username} was deleted: ${wasDeleted}`);
+  } catch (Error) {
+    res.status(404).send(Error.message);
+  }
+});
+
+// for testing checkUser method:
+/* app.get("/api/users/check", (req, res) => {
+  res.send(UserFacade.checkUser("john@turturro.com", "1234"));
+}); */
+
+UserFacade.addUser({
+  name: "John",
+  userName: "john@turturro.com",
+  password: "1234",
+  role: "user"
 });
 
 const PORT = process.env.PORT || 3333;
--- Period2/Day1/express-intro/src/facades/userFacade.ts
+++ Period2/Day1/express-intro/src/facades/userFacade.ts
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 interface IGameUser {
   name: string;
   userName: string;
@@ -8,28 +10,37 @@ interface IGameUser {
 const users: Array<IGameUser> = [];
 export default class UserFacade {
   static addUser(user: IGameUser): boolean {
-    /*Info: Import bcrypt and (npm install bcrypt) and hash before you store */
-    users.push(user);
+    bcrypt.genSalt(10, (err: any, salt: any) => {
+      bcrypt.hash(user.password, salt, (err: any, hash: string) => {
+        user.password = hash;
+        users.push(user);
+      });
+    });
     return true;
   }
+
   static deleteUser(userName: string): boolean {
     const index: number = users.findIndex(user => user.userName === userName);
-    if (index === -1) throw "A user with the given username doesn't exist.";
+    if (index === -1)
+      throw new Error("A user with the given username doesn't exist.");
     users.splice(index, 1);
     return true;
   }
+
   static getAllUsers(): Array<IGameUser> {
     return users;
   }
+
   static getUser(userName: string): IGameUser {
     const user = users.find(user => user.userName === userName);
-    if (user === undefined) throw "A user with the given username doesn't exist.";
+    if (user === undefined)
+      throw new Error("A user with the given username doesn't exist.");
     return user;
   }
+
   static checkUser(userName: string, password: string): boolean {
-    /*Use bcrypts compare method */
-    const user = users.find(user => user.userName === userName && user.password === password);
-    return user !== undefined;
+    const user = users.find(user => user.userName === userName);
+    return user !== undefined && bcrypt.compareSync(password, user.password);
   }
 }
 
