24189c9dbf635327fde23bb68bd900f54e001eea
Simon Asholt Norup <wormple12@hotmail.com>
2020-05-18 21:13:31 +0200

    Updates

---
+++ precache-manifest.3b2176034674fdb8d5821e3f82452bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9516e1989274774554696647f9eeb754",
+    "url": "/Fullstack-JS-Exercises/index.html"
+  },
+  {
+    "revision": "e95f9b2f537b6ee2c54b",
+    "url": "/Fullstack-JS-Exercises/static/css/main.fcad1ae8.chunk.css"
+  },
+  {
+    "revision": "43c9c46a4bc9c55be42d",
+    "url": "/Fullstack-JS-Exercises/static/js/2.9a8e495a.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Fullstack-JS-Exercises/static/js/2.9a8e495a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95f9b2f537b6ee2c54b",
+    "url": "/Fullstack-JS-Exercises/static/js/main.de05c74d.chunk.js"
+  },
+  {
+    "revision": "3a65c9519f488569b74b",
+    "url": "/Fullstack-JS-Exercises/static/js/runtime-main.237d8c5d.js"
+  }
+]);
